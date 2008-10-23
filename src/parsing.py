45c7dca039b2ed1c2be4a767ccfc81c4e7840dfd
tom <tom@tom-laptop>
2008-10-23 12:40:04 +0100

    Refactored to have textui.

--- src/parsing.py
+++ src/parsing.py
@@ -3,50 +3,58 @@ import re
 import threading
 import logging
 
-class Parser:
+class AbstractParser:
+    "Base class of parsers. Should not be initialized itself."
+    
+    def __init__(self):
+        self.__observers = []
+    
+    def addListener(self, callback):
+        if callback:
+            self.__observers.append(callback)
+            
+    def removeListener(self, callback):
+        if callback:
+            self.__observers.remove(callback)
+    
+    def __fireDataEvent(self):
+        if self.__listeners == []:
+            logging.warning("No callbacks listening for data.")
+            return
+        for callback in self.__listeners:
+            callback()
+            
+    def parse(self, data):
+        msg = 'parse() must be overridden by subclasses'
+        raise NotImplementedError(msg)
+                    
+class Parser(AbstractParser):
     """Stores incoming data in a buffer and parses it 
     using a regular expression."""
     
     def __init__(self, regex, callback = None):
-        self.__dataLock = threading.RLock()
-        self.__listenerLock = threading.RLock()
         self.__regex =  regex
-        
-        if not callback:
-            self.__listeners = []
-        else:
-            self.__listeners = [callback]
-        
         self.__data = ''
                 
-    def addListener(self, callback):
-        "Adds the callback to be called when data is available."
-        with self.__listenerLock:
-            self.__listeners.append(callback)
-        
-    def removeListener(self, callback):
-        with self.__listenerLock:
-            self.__listeners.remove(callback)
-            
-    def __fireDataEvent(self):
-        with self.__listenerLock:
-            with self.__dataLock:
-                if self.__listeners == []:
-                    logging.warning("No callbacks listening for data.")
-                    return
-                for callback in self.__listeners:
-                    callback()
-        
     def parse(self, text):
         "Adds text to buffer, parses it and calls callbacks."
-        with self.__dataLock:
-            self.__data += text
+        self.__data += text
         # Use the regex to search for data
-        
         
     def __str__(self):
         "Returns a string representation of the parser."
-        with self.__listenerLock:
-            with self.__dataLock:
-                return 'Data: {0}, Callbacks{1}'.format(self.__data, self.__listeners)
+        return 'Data: {0}, Callbacks{1}'.format(self.__data, self.__listeners)
             
+class DummyParser(AbstractParser):
+    "Creates dummy serial events"
+    
+    def __init__(self):
+        self.__count = 0
+    
+    def parse(self):
+        for callback in self.__observers:
+            callback([count, count + 1])
+    
+    def __str__(self):
+        return 'Callbacks: {0}, Count: {1}'.format(self.__observers, self.__count)
+    
